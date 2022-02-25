0e51fbf9cd7f974e5389d4906f4054b7770d6236
Anglebert <anglebertsh@gmail.com>
2022-02-25 08:43:10 +0200

    edited UserModel

--- src/com/hiric/Server/Model/UserModel.java
+++ src/com/hiric/Server/Model/UserModel.java
@@ -7,12 +7,12 @@ public class UserModel implements  Serializable{
     private String lastName;
     private String email;
     private String password;
-    private String telephone;
+    private Integer telephone;
 
     public UserModel(){
 
     }
-    public UserModel(Integer userId , String firstName , String lastName , String email , String password , String telephone){
+    public UserModel(Integer userId , String firstName , String lastName , String email , String password , Integer telephone){
         this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -21,5 +21,51 @@ public class UserModel implements  Serializable{
         this.telephone = telephone;
     }
 
+    public Integer getUserId() {
+        return userId;
+    }
 
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
 }
