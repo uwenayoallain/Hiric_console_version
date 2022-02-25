47a38b8de1700a9d9ad3d7d6dd18e718e45add6c
Gashugi Aderline <aderlinecarmella@gmail.com>
2022-02-25 08:40:18 +0200

    JobPostingController

--- src/com/hiric/Server/Controllers/Hiring/JobPosting.java
+++
@@ -1,4 +0,0 @@
-package com.hiric.Server.Controllers.Hiring;
-
-public class JobPosting {
-}
---
+++ src/com/hiric/Server/Controllers/Hiring/JobPostingController.java
@@ -0,0 +1,22 @@
+package com.hiric.Server.Controllers.Hiring;
+
+import com.hiric.Server.Model.JobPosting;
+import com.hiric.Server.DbController.PropertyVariables;
+
+import  java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+/**
+ *  Job Posting controller
+ * @author Aderline Gashugi
+ */
+
+public class JobPostingController {
+    public static void main(String[] args) {
+
+        public void createJobPost(String jobTitle, String jobDescription, String jobRequirements, String location, String startDate, String  duration, int salary) {
+            
+        }
+    }
+}
