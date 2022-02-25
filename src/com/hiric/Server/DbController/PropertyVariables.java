d4292b90c1d1843e182d3d06fa3cc75c6305ff44
dabagirevalens <vdabagire@gmail.com>
2022-02-25 07:44:19 +0200

    db configuration variables

---
+++ src/com/hiric/Server/DbController/PropertyVariables.java
@@ -0,0 +1,77 @@
+package com.hiric.Server.DbController;
+
+import java.io.FileWriter;
+import java.util.Properties;
+
+/**
+*@author : DABAGIRE Valens - Project Manager
+*@description: : database configuration variables
+*/
+
+public class PropertyVariables {
+    private String databaseUrl;
+    private String databaseUsername;
+    private String databasePassword;
+    private String databasePort;
+    private Long port;
+
+    public PropertyVariables(String dbUrl, String dbUsername, String dbPassword, String dbPort, Long port){
+        this.databaseUrl = dbUrl;
+        this.databaseUsername = dbUsername;
+        this.databasePassword = dbPassword;
+        this.databasePort = dbPort;
+        this.port = port;
+    }
+
+    public String getDatabaseUrl() {
+        return databaseUrl;
+    }
+
+    public void setDatabaseUrl(String databaseUrl) {
+        this.databaseUrl = databaseUrl;
+    }
+
+    public String getDatabaseUsername() {
+        return databaseUsername;
+    }
+
+    public void setDatabaseUsername(String databaseUsername) {
+        this.databaseUsername = databaseUsername;
+    }
+
+    public String getDatabasePassword() {
+        return databasePassword;
+    }
+
+    public void setDatabasePassword(String databasePassword) {
+        this.databasePassword = databasePassword;
+    }
+
+    public String getDatabasePort() {
+        return databasePort;
+    }
+
+    public void setDatabasePort(String databasePort) {
+        this.databasePort = databasePort;
+    }
+
+    public Long getPort() {
+        return port;
+    }
+
+    public void setPort(Long port) {
+        this.port = port;
+    }
+
+    public void savePropertiesInFile() throws Exception{
+        Properties properties = new Properties();
+        properties.setProperty("dbUrl", getDatabaseUrl());
+        properties.setProperty("dbUsername", getDatabaseUsername());
+        properties.setProperty("dbPassword", getDatabasePassword());
+        properties.setProperty("dbPort", getDatabasePort().toString());
+        properties.setProperty("serverPort", getPort().toString());
+
+        properties.store(new FileWriter("dbConfig.properties"), null);
+    }
+
+}
---
+++ src/com/hiric/Server/ServerMain/HiricMultiThreadedServer.java
@@ -0,0 +1,21 @@
+package com.hiric.Server.ServerMain;
+
+//packages
+import com.hiric.Server.DbController.PropertyVariables;
+
+public class HiricMultiThreadedServer {
+
+    public void startServer() throws Exception{
+        String url = "jdbc:mysql://remotemysql.com:3306/ZKZ7qI2OW3?" + "\"autoReconnect=true&useSSL=false\"";
+        String user = "ZKZ7qI2OW3";
+        String password = "pWgWkTztns";
+
+        PropertyVariables propertyVariables = new PropertyVariables(url, user, password, "3306", 1200L);
+        propertyVariables.savePropertiesInFile();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.print("Accept multiple clients");
+    }
+}
