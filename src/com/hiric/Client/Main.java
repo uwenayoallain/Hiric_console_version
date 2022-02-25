83428cb196df992dcdca842f1957394c242db3e2
abi-seth <abiheloaf@gmail.com>
2022-02-25 09:09:24 +0200

    New: configuring color customizations

--- src/com/hiric/Client/Main.java
+++ src/com/hiric/Client/Main.java
@@ -1,4 +1,15 @@
 package com.hiric.Client;
 
+import java.io.IOException;
+
 public class Main {
+
+    public static void WelcomeToHiric() {
+
+    }
+
+    public static void main(String [] args) throws IOException {
+        System.out.println("Welcome to hiric!");
+    }
+
 }
