77c1a800e17f9551741b9bcf828b3bc19640b158
Beulla <beullarugero6@gmail.com>
2022-02-25 08:49:38 +0200

    login

--- src/com/hiric/Server/Controllers/UserModuleControllers/Login.java
+++ src/com/hiric/Server/Controllers/UserModuleControllers/Login.java
@@ -1,7 +1,25 @@
 package com.hiric.Server.Controllers.UserModuleControllers;
 
+import java.io.*;
+import java.util.Properties;
+
 public class Login {
-    public static void main(String[] args){
-        System.out.println("User login");
+    public Integer UserLogin(String email,String password) throws IOException {
+        InputStream inputstream=new FileInputStream("config.properties");
+        Properties properties=new Properties();
+        Object userId=(Object) properties.getProperty("UserId");
+
+        if(userId==null){
+            InputStream input=new FileInputStream("config.properties");
+            Properties props=new Properties();
+            properties.load(input);
+            properties.setProperty("Token","");
+            properties.setProperty("UserId",String.valueOf(0));
+            
+            properties.store(new FileOutputStream("config.properties"),null);
+            return 0;
+        }
+        String ret=userId.toString();
+        return Integer.parseInt(ret);
     }
 }
